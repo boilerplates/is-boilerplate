ee29cd348b4b2c55c0200be2cf7ea426d2f4484b
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-12 19:39:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/*!
+ * is-boilerplate <https://github.com/jonschlinkert/is-boilerplate>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+var isPlainObject = require('is-plain-object');
+
+module.exports = function isBoilerplate(obj) {
+  return obj && typeof obj === 'object'
+    && !isPlainObject(obj)
+    && obj.isBoilerplate === true;
+};
---
+++ test.js
@@ -0,0 +1,16 @@
+require('mocha');
+var assert = require('assert');
+var Boilerplate = require('boilerplate');
+var isBoilerplate = require('./');
+
+describe('isBoilerplate', function () {
+  it('should return false if the value is not a Boilerplate:', function () {
+    assert(isBoilerplate('a') === false);
+    assert(isBoilerplate({}) === false);
+    assert(isBoilerplate({ files: [] }) === false);
+  });
+
+  it('should return true if the value is a Boilerplate:', function () {
+    assert(isBoilerplate(new Boilerplate({ files: [] })) === true);
+  });
+});
